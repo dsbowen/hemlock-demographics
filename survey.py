86258845e9228735a62eea0b646276493c641185
dsbowen <dsbowen@wharton.upenn.edu>
2020-10-02 21:11:51 -0400

    first draft finished

--- survey.py
+++ survey.py
@@ -1,10 +1,10 @@
 from hemlock import Branch, Page, Label, route
-from hemlock_demographics import demographics
+from hemlock_demographics import comprehensive_demographics
 
 @route('/survey')
 def start():
     return Branch(
-        Page(*demographics('gender', 'dob')),
+        comprehensive_demographics(page=True, require=True),
         Page(
             Label('<p>Hello World</p>'), 
             terminal=True
